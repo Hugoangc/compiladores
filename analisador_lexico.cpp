eb71af4d42c8abd7b49d3c3ab3306c52e5ee3790
Hugoangc <81435251+Hugoangc@users.noreply.github.com>
2024-11-05 13:40:07 -0300

    pequenas correções e ajustes na impressão dos tokens
    Agora os tokens possuem um número correspondente a ele

--- analisador_lexico.cpp
+++ analisador_lexico.cpp
@@ -8,27 +8,36 @@
 using namespace std;
 
 struct Token {
-    string tipo;
+    int tipo;
+    string lexema;
     int linha;
     int coluna;
 
-    Token(string t, int ln, int col) : tipo(t), linha(ln), coluna(col) {}
+    Token(int t, string lex, int ln, int col) : tipo(t), lexema(lex), linha(ln), coluna(col) {}
 };
 
-unordered_map<string, string> criarTabelaDeTokens() {
+unordered_map<string, int> criarTabelaDeTokens() {
     return {
-        {"+", "OP_ADD"}, {"-", "OP_SUB"}, {"*", "OP_MULT"}, {"/", "OP_DIV"}, {"%", "OP_MOD"},
-        {"==", "OP_IGUAL"}, {"!=", "OP_DIF"}, {">", "OP_MAIOR"}, {">=", "OP_MAIOR_QUE"}, {"<", "OP_MENOR"}, {"<=", "OP_MENOR_QUE"},
-        {"||", "OP_OU_LOGICO"}, {"&&", "OP_&_LOGICO"}, {"!", "OP_NAO_LOGICO"}, {"=", "ATRIBUICAO"}, 
-        {"+=", "ATRIBUICAO_ADD"}, {"-=", "ATRIBUICAO_SUB"}, {"*=", "ATRIBUICAO_MULT"}, {"/=", "ATRIBUICAO_DIV"}, 
-        {"%=", "ATRIBUICAO_MOD"}, {"int", "TIPO_INT"}, {"float", "TIPO_FLOAT"}, {"string", "TIPO_STRING"}, {"if", "CONDICIONAL_IF"}, 
-        {"else", "CONDICIONAL_ELSE"}, {"for", "LACO_FOR"},{"while","LACO_WHILE"}, {"break", "CONTROLE_BREAK"}, {"continue", "CONTROLE_CONTINUE"},
-        {"return", "CONTROLE_RETURN"}, {"system", "FUNCAO_SYSTEM"}, {"out", "FUNCAO_OUT"}, {"print", "FUNCAO_PRINT"}, {"println", "FUNCAO_PRINTLN"},
-        {"in", "FUNCAO_IN"}, {"scan", "FUNCAO_SCAN"}, {"main", "FUNCAO_MAIN"}, {"(", "PARENTESES_ABRE"}, {")", "PARENTESES_FECHA"},
-        {"{", "CHAVE_ABRE"}, {"}", "CHAVE_FECHA"}, {";", "PONTO_VIRGULA"}, {",", "VIRGULA"}, {".", "PONTO"}, {"\"", "ASPAS_DUPLAS"}, {"\'", "ASPAS_SIMPLES"},
+        {"+", 1}, {"-", 2}, {"*", 3}, {"/", 4}, {"%", 5},
+        {"==", 11}, {"!=", 12}, {">", 13}, {">=", 14}, {"<", 15}, {"<=", 16},
+        {"||", 21}, {"&&", 22}, {"!", 23}, {"=", 24},
+        {"+=", 25}, {"-=", 26}, {"*=", 27}, {"/=", 28},
+        {"%=", 29}, {"int", 31}, {"float", 32}, {"string", 33}, {"if", 41},
+        {"else", 42}, {"for", 43}, {"while", 44}, {"break", 45}, {"continue", 46},
+        {"return", 47}, {"system", 51}, {"out", 52}, {"print", 53}, {"println", 54},
+        {"in", 55}, {"scan", 56}, {"main", 57}, {"(", 61}, {")", 62},
+        {"{", 63}, {"}", 64}, {";", 65}, {",", 66}, {".", 67}, {"\"", 71}, {"\'", 72},
     };
 }
 
+void imprimirTokens(const vector<Token>& tokens) {
+    for (const auto& token : tokens) {
+        cout << "{token: " << token.tipo
+             << ", lexema: " << token.lexema
+             << ", linha: " << token.linha
+             << ", coluna: " << token.coluna << "}" << endl;
+    }
+}
 bool isInteger(const string& str) {
     size_t start = (str[0] == '-' ? 1 : 0);
     for (size_t i = start; i < str.size(); ++i) {
@@ -67,13 +76,6 @@ bool isOctal(const string& str) {
     return true;
 }
 
-void imprimirTokens(const vector<Token>& tokens) {
-    for (const auto& token : tokens) {
-        cout << "{Tipo: " << token.tipo
-             << ", Linha: " << token.linha
-             << ", Coluna: " << token.coluna << "}" << endl;
-    }
-}
 
 string lerArquivo(const string& nome_arquivo) {
     ifstream arquivo(nome_arquivo);
@@ -84,7 +86,7 @@ string lerArquivo(const string& nome_arquivo) {
     return string((istreambuf_iterator<char>(arquivo)), istreambuf_iterator<char>());
 }
 
-vector<Token> analisarCodigo(const string& codigo, const unordered_map<string, string>& tabelaDeTokens) {
+vector<Token> analiseLexica(const string& codigo, const unordered_map<string, int>& tabelaDeTokens) {
     vector<Token> tokens;
     int linha = 1, coluna = 1;
     size_t i = 0;
@@ -106,6 +108,7 @@ vector<Token> analisarCodigo(const string& codigo, const unordered_map<string, s
             i++;
             continue;
         }
+
         // Ignorar comentários de linha e bloco
         if (ch == '/' && i + 1 < codigo.size()) {
             if (codigo[i + 1] == '/') {  // Comentário de linha
@@ -126,31 +129,36 @@ vector<Token> analisarCodigo(const string& codigo, const unordered_map<string, s
                     }
                     i++;
                 }
-                i += 2;  // Pular "*/"
+                i += 2; //Pular */
                 coluna += 2;
                 continue;
             }
         }
+
         // Verificar strings e \n dentro delas
         if (ch == '"') {
             size_t start_coluna = coluna;
+            string lexema = "\"";
             i++;
             coluna++;
 
-            // Identificar conteúdo da string, separando \n
             while (i < codigo.size() && codigo[i] != '"') {
                 if (codigo.substr(i, 2) == "\\n") {
-                    tokens.emplace_back("QUEBRA_LINHA", linha, coluna);
+                    tokens.emplace_back(6, "\\n", linha, coluna);
+                    i += 2;
+                    coluna += 2;
+                } else if (codigo[i] == '\n') {
+                    throw runtime_error("Erro: string nao fechada na linha " + to_string(linha) + ", coluna " + to_string(start_coluna));
+                } else {
+                    lexema += codigo[i];
+                    i++;
+                    coluna++;
                 }
-                if (codigo[i] == '\n') {
-                    throw runtime_error("Erro: string nao fechada na linha " + to_string(linha));
-                } 
-                i++;
-                coluna++;
             }
 
             if (i < codigo.size() && codigo[i] == '"') {
-                tokens.emplace_back("STRING", linha, start_coluna);
+                lexema += "\"";
+                tokens.emplace_back(6, lexema, linha, start_coluna);
                 i++;
                 coluna++;
             } else {
@@ -158,35 +166,59 @@ vector<Token> analisarCodigo(const string& codigo, const unordered_map<string, s
             }
             continue;
         }
-        
-        if (ch == '\'') {
+        if (ch == '\'') { // Verificando aspas simples
             size_t start_coluna = coluna;
+            string lexema = "'";
             i++;
             coluna++;
 
-            // Identificar conteúdo da string, separando \n
             while (i < codigo.size() && codigo[i] != '\'') {
                 if (codigo.substr(i, 2) == "\\n") {
-                    tokens.emplace_back("QUEBRA_LINHA", linha, coluna);
+                    tokens.emplace_back(6, "\\n", linha, coluna);
+                    i += 2;
+                    coluna += 2;
+                } else if (codigo[i] == '\n') {
+                    throw runtime_error("Erro: string nao fechada na linha " + to_string(linha) + ", coluna " + to_string(start_coluna));
+                } else {
+                    lexema += codigo[i];
+                    i++;
+                    coluna++;
                 }
-                if (codigo[i] == '\n') {
-                    throw runtime_error("Erro: string nao fechada na linha " + to_string(linha));
-                } 
-                i++;
-                coluna++;
             }
 
-
-
             if (i < codigo.size() && codigo[i] == '\'') {
-                tokens.emplace_back("STRING", linha, start_coluna);
+                lexema += '\'';
+                tokens.emplace_back(6, lexema, linha, start_coluna);
                 i++;
                 coluna++;
             } else {
                 throw runtime_error("Erro: string nao fechada na linha " + to_string(linha) + ", coluna " + to_string(start_coluna));
             }
             continue;
-        }        
+        }
+
+
+        // Verificar palavras-chave e identificadores
+        if (isalpha(ch) || ch == '_') {
+            size_t start_coluna = coluna;
+            string lexema;
+
+            // Extrair a sequência de caracteres
+            while (i < codigo.size() && (isalnum(codigo[i]) || codigo[i] == '_')) {
+                lexema += codigo[i];
+                i++;
+                coluna++;
+            }
+
+            // Verificar se é uma palavra-chave
+            if (tabelaDeTokens.count(lexema) > 0) {
+                tokens.emplace_back(tabelaDeTokens.at(lexema), lexema, linha, start_coluna);
+            } else {
+                // Se não, é um identificador
+                tokens.emplace_back(99, lexema, linha, start_coluna); // 99 como código para identificador
+            }
+            continue;
+        }
 
         // Verificar números
         if (isdigit(ch) || (ch == '-' && isdigit(codigo[i + 1])) || (ch == '0' && i + 1 < codigo.size() && codigo[i + 1] == 'x')) {
@@ -227,51 +259,20 @@ vector<Token> analisarCodigo(const string& codigo, const unordered_map<string, s
                 throw runtime_error("Erro: Numero invalido na linha " + to_string(linha) + ", coluna " + to_string(start_coluna));
             }
 
-            // Classifica o tipo do número
-            if (VAR_isHexadecimal && isHexadecimal(numero)) tokens.emplace_back("NUMERO_HEXADECIMAL", linha, start_coluna);
-            else if (isOctal(numero)) tokens.emplace_back("NUMERO_OCTAL", linha, start_coluna);
-            else if (isInteger(numero)) tokens.emplace_back("NUMERO_INT", linha, start_coluna);
-            else if (isFloat(numero)) tokens.emplace_back("NUMERO_FLOAT", linha, start_coluna);
+            if (VAR_isHexadecimal && isHexadecimal(numero)) tokens.emplace_back(101, numero, linha, start_coluna); // 101: hexadecimal
+            else if (isOctal(numero)) tokens.emplace_back(102, numero, linha, start_coluna); // 102: octal
+            else if (isInteger(numero)) tokens.emplace_back(104, numero, linha, start_coluna); // 104 : int
+            else if (isFloat(numero)) tokens.emplace_back(103, numero, linha, start_coluna); // 103: float
             else throw runtime_error("Erro: Numero invalido na linha " + to_string(linha) + ", coluna " + to_string(start_coluna));
-
             continue;
         }
 
-
-
-        // Verificar identificadores e palavras-chave, tratando o ponto como um delimitador separado
-        if (isalpha(ch) || ch == '_') {
-            string lexema;
-            size_t start_coluna = coluna;
-
-            while (i < codigo.size() && (isalnum(codigo[i]) || codigo[i] == '_')) {
-                lexema += codigo[i];
-                i++;
-                coluna++;
-            }
-
-            if (tabelaDeTokens.count(lexema) > 0) {
-                tokens.emplace_back(tabelaDeTokens.at(lexema), linha, start_coluna);
-            } else {
-                tokens.emplace_back("VARIAVEL", linha, start_coluna);
-            }
-            continue;
-        }
-
-        // Tratar ponto como um token separado
-        if (ch == '.') {
-            tokens.emplace_back("PONTO", linha, coluna);
-            i++;
-            coluna++;
-            continue;
-        }
-
-        // Operadores compostos e símbolos únicos
+        // Verificar operadores e símbolos
         string op(1, ch);
         if (i + 1 < codigo.size()) {
             string op2 = op + codigo[i + 1];
             if (tabelaDeTokens.count(op2) > 0) {
-                tokens.emplace_back(tabelaDeTokens.at(op2), linha, coluna);
+                tokens.emplace_back(tabelaDeTokens.at(op2), op2, linha, coluna);
                 i += 2;
                 coluna += 2;
                 continue;
@@ -279,7 +280,7 @@ vector<Token> analisarCodigo(const string& codigo, const unordered_map<string, s
         }
 
         if (tabelaDeTokens.count(op) > 0) {
-            tokens.emplace_back(tabelaDeTokens.at(op), linha, coluna);
+            tokens.emplace_back(tabelaDeTokens.at(op), op, linha, coluna);
             i++;
             coluna++;
             continue;
@@ -291,6 +292,7 @@ vector<Token> analisarCodigo(const string& codigo, const unordered_map<string, s
     return tokens;
 }
 
+
 int main() {
     string nome_arquivo;
     cout << "Informe o nome do arquivo a ser analisado: ";
@@ -299,8 +301,8 @@ int main() {
     try {
         string codigo = lerArquivo(nome_arquivo);
         if (!codigo.empty()) {
-            unordered_map<string, string> tabelaDeTokens = criarTabelaDeTokens();
-            vector<Token> tokens = analisarCodigo(codigo, tabelaDeTokens);
+            unordered_map<string, int> tabelaDeTokens = criarTabelaDeTokens();
+            vector<Token> tokens = analiseLexica(codigo, tabelaDeTokens);
             imprimirTokens(tokens);
         }
     } catch (const runtime_error& e) {
