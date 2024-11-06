dd24e3c7f86c8fc38ec3a3483c3c71af7189cce0
Hugoangc <81435251+Hugoangc@users.noreply.github.com>
2024-11-06 01:03:42 -0300

    numero negativo

--- teste3.java
+++ teste3.java
@@ -1,10 +1,11 @@
  int main() {
-        int a = 10.12; // Comentário de linha
+        int a = -10.12; // Comentário de linha
         float b = 052; // Número octal
+        float ponto = 5.;
 
         // Testando strings e caracteres
-        string str1 = "Texto com quebra de linha\\n e outros caracteres especiais.";
-        string str2 = "Este é um teste de string com 'caracteres' entre aspas simples.";
+        string str1 = "Texto com quebra de linha\n e outros caracteres especiais.";
+        string str2 = "Este e um teste de string com 'caracteres' ent\re aspas simples.";
         
         // Teste de números
         int numeroInteiro = 42;
@@ -14,10 +15,10 @@
         // Condicional e laço
         if (a == 10) {
             b += 2.5;
-            system.out.println("Valor de a: " + a);
+            system.out.println("Valor \nde a:  + a");
             system.out.println("Valor de b: " + b);
         } else {
-            system.out.println("A variável 'a' não é igual a 10.");
+            system.out.println("A variavel 'a' nao e igual a 10.\n");
         }
 
         // Testando a função print
@@ -26,11 +27,10 @@
         /*
         Bloco de comentários
         que deve ser ignorado
-        */
+        */ int a = 20;
     }
 
     // Função de teste
     int print(string message) {
         system.out.println(message);
     }
-
