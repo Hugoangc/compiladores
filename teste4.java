fb1ad1a2fa9e55aa24a1d8e95b2ebcbe23739066
Hugoangc <81435251+Hugoangc@users.noreply.github.com>
2024-11-29 20:17:53 -0300

    atualizacao de caso teste

--- teste4.java
+++ teste4.java
@@ -1,8 +1,50 @@
+int main(){
+    int a,b,c;
+    float d,e,f;
+    string g,h,i;
+
+    for (a=15;a<5;a+=1){
+        if (a<3){
+            break;
+            continue;
+        }
+        system.out.print(a,b,d,"string");
+        system.in.scan(int, a);
+
+        while (i!='string' && i!= 10)
+            d=1;
+    }
+}
+
  int main() {
-     int a = 10;
-     if(a < 10){
-          system.out.print("Menor!");
-     }
-      string str1 = "Texto com quebra de linha\n e outros caracteres especiais.";
-      string str2 = "Este e um teste de string \tcom 'caracteres' ent\re aspas simples.";
- }
+        int a; // Comentário de linha
+        float b; // Número octal
+        float ponto56;
+
+        //float variavel_snake_case = 111b;
+        // Testando strings e caracteres
+        string a;
+       // string str2 = "Este e um teste de string \tcom 'caracteres' ent\re aspas simples.";
+        str1 = "Texto com quebra de linha\n e outros caracteres especiais.";
+        // Teste de números
+        int numeroInteiro;
+        float numeroFlutuante, numeroHexadecimal; // Número hexadecimal
+
+        // Condicional e laço
+        if (a == 10) {
+            b += 2.5;
+            system.out.print("Valor \nde a:  + a");
+            system.out.print(b, "String");
+        } else {
+            system.out.print("A variavel 'a' nao e igual a 10.\n");
+        }
+
+        // Testando a função print
+        system.out.print("Texto de teste");
+        system.out.print('Texto entre aspas \nsimples');
+        /*
+        Bloco de comentários
+        que deve ser ignorado
+        */ int a5122;
+    }
+
