a4b707fc961bc79f5cd7ffb9a145189e13767989
Hugoangc <81435251+Hugoangc@users.noreply.github.com>
2024-11-29 20:19:10 -0300

    Update and rename sss.java to teste1.java

--- sss.java
+++
@@ -1,6 +0,0 @@
-int abacate() {
-    int x = 10.5;
-    if (x > 5){
-        // código
-    }
-}
---
+++ teste1.java
@@ -0,0 +1,6 @@
+int main() {
+    int x, a, b, c;
+    system.out.print(a, "string");
+    system.in.scan(int, xyx);
+    
+}
