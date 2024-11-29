b21d98d4de9460e2b61c11f23a1e1a231d19762e
Hugoangc <81435251+Hugoangc@users.noreply.github.com>
2024-11-29 20:19:38 -0300

    Create exemplo1.java

---
+++ exemplo1.java
@@ -0,0 +1,17 @@
+int main(){
+    int a,b,c;
+    float d,e,f;
+    string g,h,i;
+
+    for (a=15;a<5;a+=1){
+        if (a<3){
+            break;
+            continue;
+        }
+        system.out.print(a,b,d,"string");
+        system.in.scan(int, a);
+
+        while (i!='string' && i!= 10)
+            d=1;
+    }
+}
